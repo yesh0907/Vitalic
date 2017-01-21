72cf661a0d911d8f5d0119c5c83fb6cb80110a47
ThunderLight25 <cogsteeler2@gmail.com>
2017-01-22 02:00:17 +0800

    Chart.js Importation

--- Frontend/src/components/detailCard.vue
+++ Frontend/src/components/detailCard.vue
@@ -10,10 +10,12 @@
       <h3 v-if="infoType == 'Breathing Rate'">Latest: {{$store.state.records['a42936c9458ddf1451f91a5831ab670c'].breathingRate}}</h3>
       <h3 v-if="infoType == 'Blood Pressure'">Latest: {{$store.state.records['a42936c9458ddf1451f91a5831ab670c'].bloodPressure}}</h3>
     </div>
+
   </el-card>
 </template>
 
 <script>
+  import Chart from 'chart.js'
   export default {
     name: 'detailCard',
     props: [ 'infoType' ],
