2ae59910d00f357ab9a1a664804f4291cf1c8ad3
tcode2k16 <tcode2k16@gmail.com>
2017-01-21 23:52:12 +0800

    cleaned up util

--- Frontend/src/util/utilities.js
+++ Frontend/src/util/utilities.js
@@ -1,92 +1,64 @@
-function isHRHealthy(HR, age) {
-	if (age < 11) {
-		if (HR < 70) {
-			return "LOW";
-		}
-		else if (HR > 160) {
-			return "HIGH";
-		}
-		else {
-			return "NORMAL";
-		}
-	}
-	else {
-		if (HR < 60) {
-			return "EXCELLENT";
-		}
-		else if (HR <= 100) {
-			return "NORMAL";
-		}
-		else {
-			return "HIGH";
-		}
-	}
+export default {
+  isHRHealthy,
+  calculateBP,
+  isBPHealthy,
+  checkChol,
+  checkStress,
+  calculateBreathingRate
 }
 
-function calculateBP(HR, gender, age) {
-	var R = 18.5;
-	var Q = 0;
-	if(gender === "MALE") {
-		Q = 5.0;
-	} else {
-		Q = 4.5;
-	}
-	var ejectionTIme = 364.5 - (1.23 * HR);
-	var bodySurfaceArea = 0.007184*(Math.pow(137,0.425))*(Math.pow(66,0.725));
-	var strokeVolume = -6.6 + 0.25*(ejectionTime-35) - 0.62*HR + 40.4*bodySurfaceArea - 0.51*age;
-    var pulsePressure = Math.abs(strokeVolume / ((0.013*137 - 0.007*age-0.004*HR)+1.307));
-    var meanPulsePressure = Q*R;
-
-    var systolicPressure = Math.round(meanPulsePressure + 4.5/3*pulsePressure);
-    var diastolicPressure = Math.round(meanPulsePressure - pulsePressure/3);
-
-    var bloodPressure = {};
-    bloodPressure["Systolic Pressure"] = systolicPressure;
-    bloodPressure["Diastolic Pressure"] = diastolicPressure;
-
-    return bloodPressure;
+function isHRHealthy (HR, age) {
+  if (age < 11) {
+    if (HR < 70) return 'LOW'
+    else if (HR > 160) return 'HIGH'
+    else return 'NORMAL'
+  } else {
+    if (HR < 60) return 'EXCELLENT'
+    else if (HR <= 100) return 'NORMAL'
+    else return 'HIGH'
+  }
 }
 
-function isBPHealthy(bloodPressure) {
-	var systolicPressure = bloodPressure["Systolic Pressure"];
-    var diastolicPressure = bloodPressure["Diastolic Pressure"];
+function calculateBP (HR, gender, age) {
+  let R = 18.5
+  let Q = gender === 'MALE' ? 5.0 : 4.5
 
-    if (systolicPressure < 90 || diastolicPressure < 60) {
-   		return "LOW";
-    }
-    else if (systolicPressure > 120 || diastolicPressure > 80) {
-   		return "HIGH";
-   	}
-   	else {
-   		return "NORMAL";
-   	}
+  let ejectionTime = 364.5 - (1.23 * HR)
+  let bodySurfaceArea = 0.007184 * (Math.pow(137, 0.425)) * (Math.pow(66, 0.725))
+  let strokeVolume = -6.6 + 0.25 * (ejectionTime - 35) - 0.62 * HR + 40.4 * bodySurfaceArea - 0.51 * age
+  let pulsePressure = Math.abs(strokeVolume / ((0.013 * 137 - 0.007 * age - 0.004 * HR) + 1.307))
+  let meanPulsePressure = Q * R
+
+  let systolicPressure = Math.round(meanPulsePressure + 4.5 / 3 * pulsePressure)
+  let diastolicPressure = Math.round(meanPulsePressure - pulsePressure / 3)
+
+  return {
+    systolicPressure,
+    diastolicPressure
+  }
 }
 
-function checkChol(bloodPressureHealth, heartRateHealth) {
-	if (bloodPressureHealth === "HIGH" && heartRateHealth !== "HIGH") {
-		return "HIGH";
-	}
-	return "NORMAL";
+function isBPHealthy (bloodPressure) {
+  let systolicPressure = bloodPressure.systolicPressure
+  let diastolicPressure = bloodPressure.diastolicPressure
+
+  if (systolicPressure < 90 || diastolicPressure < 60) return 'LOW'
+  else if (systolicPressure > 120 || diastolicPressure > 80) return 'HIGH'
+  else return 'NORMAL'
 }
 
-function checkStress(bloodPressureHealth) {
-	return "HIGH" === bloodPressureHealth ? "HIGH":"NORMAL";
+function checkChol (bloodPressureHealth, heartRateHealth) {
+  return (bloodPressureHealth === 'HIGH' && heartRateHealth !== 'HIGH') ? 'HIGH' : 'NORMAL'
 }
 
-function calculateBreathingRate(age) {
-	if (age < 6) {
-		return (Math.floor(Math.random() * 31) + 20);
-	}
-	else if (age < 11) {
-		return (Math.floor(Math.random() * 16) + 15);
-	}
-	else if (age < 15) {
-		return (Math.floor(Math.random() * 9) + 12);
-	}
-	else if (age <= 20) {
-		return (Math.floor(Math.random() * 19) + 12);
-	}
-	else {
-		return (Math.floor(Math.random() * 5) + 16);
-	}
+function checkStress (bloodPressureHealth) {
+  return bloodPressureHealth === 'HIGH' ? 'HIGH' : 'NORMAL'
+}
+
+function calculateBreathingRate (age) {
+  if (age < 6) return (Math.floor(Math.random() * 31) + 20)
+  else if (age < 11) return (Math.floor(Math.random() * 16) + 15)
+  else if (age < 15) return (Math.floor(Math.random() * 9) + 12)
+  else if (age <= 20) return (Math.floor(Math.random() * 19) + 12)
+  else return (Math.floor(Math.random() * 5) + 16)
 }
