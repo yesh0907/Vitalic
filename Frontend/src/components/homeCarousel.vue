76235bde5388bba4d0033365739bd6a1a081abec
ThunderLight25 <cogsteeler2@gmail.com>
2017-01-21 23:44:37 +0800

    Encapsulating Carousel

---
+++ Frontend/src/components/homeCarousel.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="homeCarousel">
+   <el-carousel :interval="3000" height="200px">
+     <el-carousel-item>
+       <h3>Heart Rate: <h2>{{ $store.state.records['a42936c9458ddf1451f91a5831ab670c'].heartrate }}</h2></h3>
+     </el-carousel-item>
+     <el-carousel-item>
+       <h3>Stress Level: <h2>{{ ($store.state.records['a42936c9458ddf1451f91a5831ab670c'].stressLevel).charAt(0).toUpperCase() + ($store.state.records['a42936c9458ddf1451f91a5831ab670c'].stressLevel).substring(1) }}</h2></h3>
+     </el-carousel-item>
+     <el-carousel-item>
+       <h3>Breathing Rate: <h2>{{ $store.state.records['a42936c9458ddf1451f91a5831ab670c'].breathingRate }}</h2></h3>
+     </el-carousel-item>
+     <el-carousel-item>
+       <h3>Blood Pressure: <h2>{{ $store.state.records['a42936c9458ddf1451f91a5831ab670c'].bloodPressure }}</h2></h3>
+     </el-carousel-item>
+  </el-carousel>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'homeCarousel',
+    data () {
+      return {
+        activeName: 'first'
+      }
+    },
+  }
+</script>
+
+<style>
+.item {
+  margin-top: 10px;
+  margin-right: 40px;
+}
+
+</style>
