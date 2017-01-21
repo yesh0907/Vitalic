9872f294041a18c603201ddc339df04b734d71fd
ThunderLight25 <cogsteeler2@gmail.com>
2017-01-22 03:49:35 +0800

    Fixed width of Homepage and Navbar

--- Frontend/src/components/Navbar.vue
+++ Frontend/src/components/Navbar.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="navbar">
     <el-row>
-      <el-col :span="6"><router-link to="/"><img id="logo" src="../assets/v_logo_v.png"/></router-link></el-col>
-      <el-col class="menu" :span="6" style="float: right;">
+      <el-col :span="6" :offset="4"><router-link to="/"><img id="logo" src="../assets/v_logo_v.png"/></router-link></el-col>
+      <el-col class="menu" :span="6" :offset="5" style="">
         <el-menu default-active="1" class="el-menu-demo" mode="horizontal" @select="handleSelect" style="float: left">
           <el-menu-item index="1"><router-link to="/" class="route">Home</router-link></el-menu-item>
           <el-menu-item index="2"><router-link to="About" class="route">About</router-link></el-menu-item>
--- Frontend/src/components/homeInfo.vue
+++ Frontend/src/components/homeInfo.vue
@@ -1,16 +1,18 @@
 <template>
-  <el-card>
-    <div slot="header" class="clearfix">
-      <span style="line-height: 36px; color: #e74c3c"><h1>Welcome to Vitalic</h1></span>
-    </div>
-    <div>
-    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-    </p>
-    <router-link to="About"><el-button type="primary">Learn More!</el-button></router-link>
-    </div>
-
-  </el-card>
-
+  <el-row>
+    <el-col :span="16" :offset="4">
+      <el-card id="homeInfo">
+        <div slot="header" class="clearfix">
+          <span style="line-height: 36px; color: #e74c3c"><h1>Welcome to Vitalic</h1></span>
+        </div>
+        <div>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        </p>
+        <router-link to="About"><el-button type="primary">Learn More!</el-button></router-link>
+        </div>
+      </el-card>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
@@ -20,5 +22,8 @@
 </script>
 
 <style>
+  #homeInfo {
+    max-width: 1000px;
 
+  }
 </style>
