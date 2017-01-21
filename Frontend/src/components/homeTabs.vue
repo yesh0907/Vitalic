2570a65a627f884450546d07fb1ca67a757eb5fe
ThunderLight25 <cogsteeler2@gmail.com>
2017-01-21 23:37:40 +0800

    HomeTabs & Home.vue restored

---
+++ Frontend/src/components/homeTabs.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="homeTabs">
+    <el-tabs class="tab" type="border-card" @tab-click="handleClick">
+     <el-tab-pane label="LiveStream"></el-tab-pane>
+     <el-tab-pane label="Latest Results">
+       <el-carousel :interval="3000" height="200px">
+         <el-carousel-item>
+           <h3>Heart Rate: <h2>{{ $store.state.records['a42936c9458ddf1451f91a5831ab670c'].heartrate }}</h2></h3>
+         </el-carousel-item>
+         <el-carousel-item>
+           <h3>Stress Level: <h2>{{ ($store.state.records['a42936c9458ddf1451f91a5831ab670c'].stressLevel).charAt(0).toUpperCase() + ($store.state.records['a42936c9458ddf1451f91a5831ab670c'].stressLevel).substring(1) }}</h2></h3>
+         </el-carousel-item>
+         <el-carousel-item>
+           <h3>Breathing Rate: <h2>{{ $store.state.records['a42936c9458ddf1451f91a5831ab670c'].breathingRate }}</h2></h3>
+         </el-carousel-item>
+         <el-carousel-item>
+           <h3>Blood Pressure: <h2>{{ $store.state.records['a42936c9458ddf1451f91a5831ab670c'].bloodPressure }}</h2></h3>
+         </el-carousel-item>
+      </el-carousel>
+     </el-tab-pane>
+    </el-tabs>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'homeTabs',
+    data () {
+      return {
+        activeName: 'first'
+      }
+    },
+    methods: {
+      handleClick (tab, event) {
+        console.log(tab, event)
+      }
+    }
+  }
+</script>
+
+<style>
+.item {
+  margin-top: 10px;
+  margin-right: 40px;
+}
+
+</style>
--- Frontend/src/views/Home.vue
+++ Frontend/src/views/Home.vue
@@ -1,21 +1,48 @@
 <template>
   <div id="home">
-    <el-row><el-col :span="24"><img id="logo" src="../assets/v_logo_v.png"/></el-col></el-row>
-    <el-row><el-col :span="12"><button></button></el-row>
-    <!-- <router-link to="/Login"></router-link> -->
+    <el-row>
+      <el-col :span="6"><img id="logo" src="../assets/v_logo_v.png"/></el-col>
+      <el-col :offset="12"></el-col>
+    </el-row>
+    <br>
+    <homeTabs></homeTabs>
   </div>
 </template>
 
 <script>
+  import homeTabs from '../components/homeTabs'
   export default {
-    name: 'home'
+    name: 'home',
+    components: {
+      homeTabs
+    },
+    data () {
+      return {
+        activeName: 'first'
+      }
+    },
+    methods: {
+      handleClick (tab, event) {
+        console.log(tab, event)
+      }
+    }
   }
 </script>
 
 <style>
 #logo {
-  height: 50vh;
-  width: 150vh;
+  height: 15vh;
+  width: 50vh;
   overflow: hidden;
 }
+
+.item {
+  margin-top: 10px;
+  margin-right: 40px;
+}
+
+.tab {
+
+}
+
 </style>
