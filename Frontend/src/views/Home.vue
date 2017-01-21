f45030cf191eeb4b708ce31e4cd00fa596b67d75
ThunderLight25 <cogsteeler2@gmail.com>
2017-01-22 02:18:36 +0800

    About Page Updated

--- Frontend/src/views/Home.vue
+++ Frontend/src/views/Home.vue
@@ -12,12 +12,17 @@
     <br>
     <el-card>
       <div slot="header" class="clearfix">
-        <span style="line-height: 36px;"><h1>About Us</h1></span>
+        <span style="line-height: 36px; color: #e74c3c"><h1>About</h1></span>
       </div>
-      <h4></h4>
+      <h4>About Our Product</h4>
       <p>
         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
       </p>
+      <h4>About The Team</h4>
+      <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+      </p>
+      
     </el-card>
     <!-- <homeTabs></homeTabs>
     <br>
