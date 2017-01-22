3d4b7c605d641784fdcfee1c233fad3f957becb9
ThunderLight25 <cogsteeler2@gmail.com>
2017-01-22 08:30:46 +0800

    Added Charts, Home & About Descriptions (that are not lorem ipsum)

--- Frontend/src/components/detailCard.vue
+++ Frontend/src/components/detailCard.vue
@@ -4,23 +4,39 @@
       <span style="line-height: 36px;">{{infoType}}</span>
     </div>
     <div>
-      <detailType infoType="this.infoType"></detailType>
-      <h3 v-if="infoType == 'Heart Rate'">Latest: {{$store.state.records['a42936c9458ddf1451f91a5831ab670c'].heartrate}}</h3>
-      <h3 v-if="infoType == 'Stress Level'">Latest: {{$store.state.records['a42936c9458ddf1451f91a5831ab670c'].stressLevel.charAt(0).toUpperCase() + $store.state.records['a42936c9458ddf1451f91a5831ab670c'].stressLevel.substring(1)}}</h3>
-      <h3 v-if="infoType == 'Breathing Rate'">Latest: {{$store.state.records['a42936c9458ddf1451f91a5831ab670c'].breathingRate}}</h3>
-      <h3 v-if="infoType == 'Blood Pressure'">Latest: {{$store.state.records['a42936c9458ddf1451f91a5831ab670c'].bloodPressure}}</h3>
+      <!-- <detailType infoType="this.infoType"></detailType> -->
+      <div  v-if="infoType == 'Heart Rate'">
+        <h3>Latest: {{$store.state.records['a42936c9458ddf1451f91a5831ab670c'].heartrate}}</h3>
+        <chartComponent typeOfData="Heart Rate"></chartComponent>
+      </div>
+      <div v-if="infoType == 'Stress Level'">
+        <h3>Latest: {{$store.state.records['a42936c9458ddf1451f91a5831ab670c'].stressLevel.charAt(0).toUpperCase() + $store.state.records['a42936c9458ddf1451f91a5831ab670c'].stressLevel.substring(1)}}</h3>
+        <chartComponent typeOfData="Stress Level"></chartComponent>
+      </div>
+      <div v-if="infoType == 'Breathing Rate'">
+        <h3>Latest: {{$store.state.records['a42936c9458ddf1451f91a5831ab670c'].breathingRate}}</h3>
+        <chartComponent typeOfData="Breathing Rate"></chartComponent>
+      </div>
+      <div v-if="infoType == 'Blood Pressure'">
+        <h3>Latest: {{$store.state.records['a42936c9458ddf1451f91a5831ab670c'].bloodPressure}}</h3>
+        <chartComponent typeOfData="Blood Pressure"></chartComponent>
+      </div>
+    </div>
+    <div>
     </div>
   </el-card>
 </template>
 
 <script>
-  // import Chart from 'chart.js'
+  import chartComponent from './chartComponent'
   export default {
     name: 'detailCard',
     props: [ 'infoType' ],
+    components: {
+      chartComponent
+    },
     data () {
       return {
-
       }
     }
   }
--- Frontend/src/components/homeInfo.vue
+++ Frontend/src/components/homeInfo.vue
@@ -6,7 +6,7 @@
           <span style="line-height: 36px; color: #e74c3c"><h1>Welcome to Vitalic</h1><img id="smallPic" src="../assets/smallLogo.png"/></span>
         </div>
         <div>
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        <p>Health is not only one of the most promising applications for technology, but also one of the most beneficial applications for society. Vitalic realizes the immense potential of technology and its versatile ability to weave into many fields to benefit the masses. With our web-based application, Vitalic is able to offer basic health services previously only available through expensive pieces of hardware. Our program uses the latest Eulerian Video Magnification technologies, developed at MIT, to determine useful dynamic qualities at a point in time - including Heart Rate, Breathing Rate, and Blood Pressure. When our program was tested for accuracy against a branded Heart Rate Tracker, there was a negligible difference between the measured Heart Rates.
         </p>
         <router-link to="About"><el-button type="primary">Learn More!</el-button></router-link>
         </div>
